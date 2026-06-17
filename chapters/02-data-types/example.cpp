f909479ea0241d8f8aa327d7013e6dacc030fb07
Jivesh-2231 <jivesh2231@gmail.com>
2026-06-17 23:11:03 +0530

    Chapter 02 - Fundamentals of Data Types

---
+++ chapters/02-data-types/example.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+int main(){
+    int apple_count{15};
+    int orange_count{15};
+    int total_fruits{apple_count + orange_count};
+
+    std::cout << "number of apples " << apple_count << std::endl;
+    std::cout << "number of orange " << orange_count << std::endl;
+    std::cout << "number of total fruits " << total_fruits << std::endl;
+
+    int counter{}; // Zero intialize
+
+    //fixed variable with constant keyword
+    const unsigned toe_count{10};
+
+
+    //integer - decimal literal
+    unsigned long age {30UL}; //here UL are suffix for U- unsigned and L- long
+
+    //hexadecimal literal
+    
+}
